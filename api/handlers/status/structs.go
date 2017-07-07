4dd4b325e3eef4b2b6fe4f1b0994af3a1325d0bd
Mikun <mikun800527@gmail.com>
2017-07-07 08:48:49 +0800

    Fix status field tag

--- api/handlers/status/structs.go
+++ api/handlers/status/structs.go
@@ -8,11 +8,7 @@ type Status struct {
 	Administered *bool `json:"administered,omitempty"`
 	Blog         struct {
 		Posts struct {
-<<<<<<< HEAD
 			Amount int `json:"amount"`
-=======
-			Amount int `json:"int"`
->>>>>>> da1474513963a90bd1176e353ded31eeb3c42dfc
 		} `json:"posts"`
 	} `json:"blog"`
 }
